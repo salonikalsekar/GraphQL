44bdf70b56d4ff5cf1ab964521f78959abff9b84
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-08-05 01:42:31 -0600

    relation

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 const graphqlhttp = require('express-graphql');
 const mongoose = require('mongoose');
 const eventModel = require('./models/event');
+const userModel = require('./models/users');
+
 const {
     buildSchema
 } = require('graphql');
 
+const bcryptjs = require('bcryptjs');
 const app = express()
 
-app.get('/', (req, res, next) => {
-    res.send("hello")
-})
 
 //String-  will return a string and never a null
 //! - required
@@ -26,9 +26,21 @@ app.use('/graphql', graphqlhttp({
         date: String!
     }
 
+    type User{
+        _id: ID!
+        email: String!
+        password: String
+    }
+
     type RootQuery{
         events: [Event!]!
     }
+
+    input UserInput{
+        email: String!
+        password: String
+    }
+
     input EventInput{
         title: String!
         description: String!
@@ -37,6 +49,7 @@ app.use('/graphql', graphqlhttp({
     }
     type RootMutation{
         createEvent(eventInp : EventInput): Event
+        createUser(userInp : UserInput): User
 
     }
         schema{
@@ -54,24 +67,51 @@ app.use('/graphql', graphqlhttp({
                 throw err;
             })
         },
-        createEvent: (args) =>{
-            // const event ={
-            //     _id :  Math.random().toString(),
-            //     title: args.eventInp.title,
-            //     description: args.eventInp.description,
-            //     price: +args.eventInp.price,
-            //     date: new Date().toISOString()
-            // }
-            // events.push(event)
-
+        createEvent: args => {
             const event = new eventModel({
-                title: args.eventInp.title,
-                    description: args.eventInp.description,
-                    price: +args.eventInp.price,
-                    date: new Date().toISOString()
+              title: args.eventInp.title,
+              description: args.eventInp.description,
+              price: +args.eventInp.price,
+              date: new Date().toISOString(),
+              creator: '5d47d75c97d0696c218aec72'
+            });
+            let createdEvent;
+            return event
+              .save()
+              .then(result => {
+                createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                return userModel.findById('5d47d75c97d0696c218aec72');
+              })
+              .then(user => {
+                if (!user) {
+                  throw new Error('User not found.');
+                }
+                console.log(event)
+                user.createdEvents.push(event);
+                return user.save();
+              })
+              .then(result => {
+                return createdEvent;
+              })
+              .catch(err => {
+                console.log(err);
+                throw err;
+              });
+          },
+        createUser: (args) =>{
+           return userModel.findOne({email: args.userInp.email}).then(user => {
+                if(user)
+                throw new Error('User already present');
+                return bcryptjs.hash(args.userInp.password, 12)
             })
-            return event.save().then(res =>{
-                return { ...res._doc, _id: res._doc._id.toString() } 
+            .then(hashed => {
+                const user = new userModel({
+                    email: args.userInp.email,
+                        password: hashed
+                })
+                        return user.save()
+                }).then(res =>{
+                    return { ...res._doc, _id: res._doc._id.toString(), password: null} 
             }).catch(err => {
                 throw err;
             })
@@ -80,8 +120,8 @@ app.use('/graphql', graphqlhttp({
     },
     graphiql : true
 }))
-mongoose.connect(`mongodb+srv://user:password@cluster0-wgbmh.mongodb.net/react-graphql-dev?retryWrites=true&w=majority`).then(() =>{
-    app.listen(3000)
+mongoose.connect(`mongodb+srv://saloni:Hari@cluster0-wgbmh.mongodb.net/react-graphql-dev?retryWrites=true&w=majority`).then(() =>{
+    app.listen(3001)
 })
 .catch(err =>{
     console.log(err)
