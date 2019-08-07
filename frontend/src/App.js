f19122d7ad7434957219cee14d08410ffadb040e
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-08-06 22:25:46 -0600

    navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import AuthPage from './pages/Auth'
+import EventsPage from './pages/Events'
+import BookingsPage from './pages/Bookings'
+import MainNavigation from './components/Navigation/MainNavigation'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <React.Fragment>
+      <MainNavigation />
+      <main className="main-content">
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </main>
+    </React.Fragment>
+  </BrowserRouter>
+  );
+}
+
+export default App;
